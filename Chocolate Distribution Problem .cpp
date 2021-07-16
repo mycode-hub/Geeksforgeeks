8ffe990a0d7fa7bcd8473ab41f41879c0571d9d6
VAIBHAV SINGHAL <v.singhal202@gmail.com>
2021-07-16 23:08:09 +0530

    Create Chocolate Distribution Problem .cpp

---
+++ Chocolate Distribution Problem .cpp
@@ -0,0 +1,44 @@
+// { Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+ // } Driver Code Ends
+class Solution{
+    public:
+    long long findMinDiff(vector<long long> A, long long n, long long k){
+    //code
+        sort(A.begin(), A.end());
+        int i = 0 , j = k-1;
+        long long  diff = LONG_MAX;
+        while( j < n)
+        {
+            diff = min( diff, (A[j] -A[i]));
+            i++; j++;
+        }
+        return diff;
+    }   
+};
+
+// { Driver Code Starts.
+int main() {
+	long long t;
+	cin>>t;
+	while(t--)
+	{
+		long long n;
+		cin>>n;
+		vector<long long> a;
+		long long x;
+		for(long long i=0;i<n;i++)
+		{
+			cin>>x;
+			a.push_back(x);
+		}
+		
+		long long m;
+		cin>>m;
+		Solution ob;
+		cout<<ob.findMinDiff(a,n,m)<<endl;
+	}
+	return 0;
+}  // } Driver Code Ends
